95e3d63f83e26efae8b9c3c26427d0212f77fa6c
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-08-21 13:57:48 -0500

    deleted app2.js and replaced it with a modified version of app3.js

--- app3.js
+++ app3.js
@@ -2,10 +2,7 @@
 
 let totalScore = 0;
 
-console.log('app is running');
-
 let guest = prompt('Welcome to my page! what is your name?');
-console.log('hi ' + guest + ', My name is Megan');
 alert('Nice to meet you, ' + guest + ' My name is Megan!');
 
 if (guest === null) {
@@ -13,7 +10,6 @@ if (guest === null) {
   user = prompt('what is your real name?');
 
 }
-console.log(guest);
 
 let game = alert('We are going to play a guessing game where I will tell you something about myself, and you must type yes for true or no for false');
 
@@ -43,17 +39,14 @@ function manyQuestions() {
     if (response === answers[i]) {
       alert('You are correct!');
       totalScore++;
-      console.log('Correct!');
     } 
     if (response !== answers[i]) {
       alert('That is incorrect!');
-      console.log('incorrect');
     } 
   }
 }
 
 manyQuestions();
-
 
 function favNumber () {
   let answer = 7;
@@ -61,7 +54,6 @@ function favNumber () {
 
   while (numberofattempts > 0) {
     let userAnswer = prompt("Guess what my favorite number is between 1 and 10!");
-    console.log("this is the user answer: " + userAnswer);
     numberofattempts--;
     if (userAnswer == answer) {
       alert("You are correct!");
@@ -78,7 +70,6 @@ function favNumber () {
 }
 favNumber();
 
-
 function favFood() {
 
   let favFoods = ["tacos", "sushi", "burgers", "pizza"];
@@ -90,8 +81,6 @@ function favFood() {
 
     for (let i = 0; i < favFoods.length; i++) {
       if (foodGuess === favFoods[i]) {
-        console.log('this is foodGuess' + foodGuess);
-        console.log('this is favefoods' + favFoods[i]);
         alert("That is correct.");
         totalScore += 1;
         attempts = 0;
@@ -115,7 +104,6 @@ favFood();
 
 
 alert("you got a total of " + totalScore + ' points');
-console.log(totalScore);
 
 alert("Thanks for playing! " + guest);
 
